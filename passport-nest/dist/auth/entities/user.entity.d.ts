448e114e12a41a9fa45f804406e8c1c25035072f
Subhashish Nabajja <subhashishnabajja1575643@gmail.com>
2025-03-14 21:54:43 +0530

    Remove dist.

--- passport-nest/dist/auth/entities/user.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class User {
-    id: string;
-    firstName: string;
-    lastName: string;
-    email: string;
-    password: string;
-    hashedRefreshToken: string;
-    private hashPassword;
-}
